fb1301608162d2abf7f99dff952a715f8864cd79
Loris_Clivaz <Loris_Clivaz@Loris_Clivaz-HP>
2018-03-09 11:10:14 +0100

    first commit

---
+++ Git/src/TestGit/TestGit.java
@@ -0,0 +1,20 @@
+/*
+* GIt
+* Author: Clivaz Loris
+* Date creation: 9 mars 2018
+* 
+*/
+/**
+ * 
+ */
+package TestGit;
+
+/**
+ * @author Loris_Clivaz
+ *
+ */
+public class TestGit {
+
+	private String gitFromScratch;
+	
+}
